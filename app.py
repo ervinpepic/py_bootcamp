50920cb50d913eefe6532f773422a76e0a50b3d8
Ervin Pepic <ervin.hack@gmail.com>
2020-03-18 00:25:43 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,31 @@
+# print('Ervin Pepic')
+# print('o----')
+# print(' ||||')
+# # print('*' * 10)
+
+######variables
+
+price = 10 #(integer)
+price = 20
+rating = 4.2 #(floating point number or float)
+name = 'Ervin' #(string)
+is_true = True | False #(true or false boolean type)
+print(price, rating, name, is_true)
+
+
+##task for variables
+
+full_name = 'John Smith'
+age = 20
+is_new = True
+
+
+###Accept input from user
+
+in_name = input('What is your name? ')
+print('Hello, ' + in_name)
+
+##task for input
+
+color = input('What is your fav color? ')
+print(in_name + ' likes ' + color)
