6299e14e997a4f95849b39c9407630c5bdd6eb0b
Ervin Pepic <ervin.hack@gmail.com>
2020-03-22 22:09:52 +0100

    finished chapters COMMAND FLOW

--- nested_loops.py
+++ nested_loops.py
@@ -1,3 +1,17 @@
 for x in range(2):
     for y in range(10):
         print(f"({x}, {y})")
+
+# range is iterable, string , list also...
+
+print(type(5))
+print(type(range(3)))
+
+for z in range(3):
+    print(z)
+
+for string in "Let me learn Python":
+    print(string.split(','))
+
+for list_num in [1, 2, 3, 4, 5, 6]:
+    print(list_num)
---
+++ while_loops.py
@@ -0,0 +1,19 @@
+number = 100
+while number > 0:
+    print(number)
+    number //= 2
+    number /= 2
+    number %= 2
+
+command = ""
+while command.lower() != "quit":
+    command = input(">")
+    print("ECHO", command)
+
+
+# infinite loop if we do not set break...
+while True:
+    command = input(">")
+    print("ECHO", command)
+    if command.lower() == "quit":
+        break
