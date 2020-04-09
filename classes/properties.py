9c8b1f4f99f27d66f8985ecbb91be82da883e747
Ervin Pepic <ervin.hack@gmail.com>
2020-04-09 22:53:11 +0200

    Fixed classes using property decorator

--- classes/properties.py
+++ classes/properties.py
@@ -1,21 +1,20 @@
 class Product:
 
     def __init__(self, fn_price):
-        self.set_price(fn_price)
+        self.price = fn_price
 
-    def get_price(self):
+    @property
+    def price(self):
         return self.__price
-    
-    def set_price(self, value):
+
+    @price.setter
+    def price(self, value):
         if value < 0:
             raise ValueError("Can't accept negative price...")
         self.__price = value
-    
-    price = property(get_price, set_price)
 
-new_product = Product(10)
+
+new_product = Product(-14)
 print(new_product.price)
 
-# in the concept above, we have a lot of confusing sutff. So first
-# we don't use pythonic way of solving problems. and we need to fix that
-# using @property decorator and @setter decorator
+# No we use a much better solution with pythonic solution using @property decorator
