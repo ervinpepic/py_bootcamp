57ba561e18bcafb260a2599f8029dbd2bb366599
Ervin Pepic <ervin.hack@gmail.com>
2020-05-05 16:09:39 +0200

    Security fixses

--- PyText/main_file.py
+++ PyText/main_file.py
@@ -1,7 +1,6 @@
 from twilio.rest import Client
-from PyText.configuration import acc_sid, auth_token
-
-client = Client(acc_sid, auth_token)
+from PyText import config
+client = Client(config.acc_sid, config.auth_token)
 call = client.messages(
     to="some number",
     from_="fffff",
